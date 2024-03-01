f9de89e637c496d9854554d5c68efc70857149e0
kopz9 <gustavoeduardo438@gmail.com>
2024-03-01 16:36:41 -0300

    feat: api

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,7 +5,7 @@ function SearchBar() {
   const [artist, setArtist] = useState("");
   const [artists, setArtists] = useState([]);
   const [hiredArtists, setHiredArtists] = useState([]);
-  const [selectedDate, setSelectedDate] = useState('');
+  const [selectedDate, setSelectedDate] = useState("");
 
   function handleSearch(e) {
     e.preventDefault();
@@ -14,7 +14,7 @@ function SearchBar() {
       return;
     }
 
-    // Verificar se a data já foi selecionada
+    // Check if the date was already used
     if (hiredArtists.some((hiredArtist) => hiredArtist.data === selectedDate)) {
       alert("Artista já contratado nesta data");
       return;
@@ -38,35 +38,44 @@ function SearchBar() {
 
   return (
     <>
-      <div className="h-screen w-screen bg-indigo-500 ">
-        <h2>Spotify API</h2>
-        <input
-          type="text"
-          value={artist}
-          onChange={(e) => setArtist(e.target.value)}
-        />
-        <form onSubmit={handleSearch}>
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            type="submit"
-          >
-            Buscar
-          </button>
-        </form>
+      <div className="h-screen flex flex-col justify-center items-center ">
+        <h2 className="text-black text-2xl mt-7 mb-1">Spotify API</h2>
+        <div className="flex items-center">
+          <input
+            className="border-2 border-gray-300 bg-white h-8 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+            type="text"
+            value={artist}
+            onChange={(e) => setArtist(e.target.value)}
+          />
+          <form onSubmit={handleSearch} className="ml-2">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              type="submit"
+            >
+              Buscar
+            </button>
+          </form>
+        </div>
         {artists.length === 0 ? (
-          <p>Digite o nome de um artista na barra de busca!</p>
+          <p className="text-white mt-8">
+            Digite o nome de um artista na barra de busca!
+          </p>
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8">
             {artists.map((artist, index) => (
-              <div key={index} className="bg-white p-4 rounded shadow">
+              <div
+                key={index}
+                className="bg-white p-4 rounded shadow flex flex-col"
+              >
                 {artist.data.visuals &&
                   artist.data.visuals.avatarImage &&
                   artist.data.visuals.avatarImage.sources[0] && (
                     <img
                       src={artist.data.visuals.avatarImage.sources[0].url}
                       alt=""
-                      className="w-full h-82 object-cover mb-2 rounded"
+                      className="w-full h-48 object-cover mb-2 rounded"
                       loading="lazy"
+                      style={{ objectFit: "cover" }}
                     />
                   )}
                 {artist.data.profile && artist.data.profile.name && (
@@ -76,10 +85,19 @@ function SearchBar() {
                 )}
                 <Link
                   to="/form"
-                  className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                  className="mt-auto inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center"
                   onClick={() => {
-                    setHiredArtists([...hiredArtists, { hiredArtist: artist.data.profile.name, date: selectedDate }]);
-                    localStorage.setItem("selectedArtist", JSON.stringify(artist.data.profile));
+                    setHiredArtists([
+                      ...hiredArtists,
+                      {
+                        hiredArtist: artist.data.profile.name,
+                        date: selectedDate,
+                      },
+                    ]);
+                    localStorage.setItem(
+                      "selectedArtist",
+                      JSON.stringify(artist.data.profile)
+                    );
                   }}
                 >
                   Contratar
