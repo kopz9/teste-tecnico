da6431b0667f89e20d6f17232be1be87d9aba113
kopz9 <gustavoeduardo438@gmail.com>
2024-03-01 02:00:43 -0300

    feat: storing form data into localstorage

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,25 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export const Form = () => {
   const selectedArtist = JSON.parse(localStorage.getItem("selectedArtist"));
   const [formData, setFormData] = useState({
-    contratante: '',
-    cache: '',
-    dataEvento: '',
-    enderecoEvento: ''
+    contratante: "",
+    cache: "",
+    dataEvento: "",
+    enderecoEvento: "",
   });
+
+  const [formDataList, setFormDataList] = useState(() => {
+    const savedFormDataList = localStorage.getItem("formDataList");
+    return savedFormDataList ? JSON.parse(savedFormDataList) : [];
+  });
+
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("formDataList", JSON.stringify(formDataList));
+  }, [formDataList]);
 
   const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
-  const handleSubmit = () => {
-    localStorage.setItem('formData', JSON.stringify(formData));
-    // Aqui você também pode enviar os dados para o servidor se necessário
-    console.log(formData);
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    
+    // Verificar se algum campo está vazio
+    if (
+      formData.contratante.trim() === "" ||
+      formData.cache.trim() === "" ||
+      formData.dataEvento.trim() === "" ||
+      formData.enderecoEvento.trim() === ""
+    ) {
+      alert("Preencha todos os campos");
+      return;
+    }
+
+    // Verificar se a data já foi selecionada
+    if (formDataList.some((item) => item.dataEvento === formData.dataEvento)) {
+      alert("Artista já contratado nesta data");
+      return;
+    }
+
+    const newFormData = {
+      contratante: formData.contratante,
+      cache: formData.cache,
+      dataEvento: formData.dataEvento,
+      enderecoEvento: formData.enderecoEvento,
+    };
+    setFormDataList([...formDataList, newFormData]);
+    // Limpar os campos após o envio
+    setFormData({
+      contratante: "",
+      cache: "",
+      dataEvento: "",
+      enderecoEvento: "",
+    });
+    setShowSuccessMessage(true); // Exibir mensagem de sucesso
   };
 
   return (
@@ -31,7 +74,7 @@ export const Form = () => {
           <p>{selectedArtist.id}</p>
         </div>
       )}
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Nome do Contratante:
           <input
@@ -72,8 +115,21 @@ export const Form = () => {
             required
           />
         </label>
-        <button type="button" onClick={handleSubmit}>Enviar</button>
+        <button type="submit">Enviar</button>
       </form>
+      {showSuccessMessage && <p>Formulário enviado com sucesso!</p>}
+      <Link
+        className='inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"'
+        to="/"
+      >
+        continuar contratando
+      </Link>
+      <Link
+        className='inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"'
+        to="/hiredArtists"
+      >
+        ver artistas contratados
+      </Link>
     </div>
   );
-}
+};
